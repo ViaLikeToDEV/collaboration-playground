56d1ce5bc6c4ae6b0b9e6977b1f650b86bd301ee
Ratrawe Lha-ong <vijvijkk82@gmail.com>
2026-03-24 23:41:50 +0700

    complete docker container + profile

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,13 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/team/{name}', function ($student_id) {
+    $profileImageUrl = asset('image-profile/' . strtolower($student_id) . '.jpg'); // ปรับเป็นตัวเล็กให้หมดลดปัญหา Case-sensitive
+
+    if (View::exists("team.$student_id")) {
+        return view("team.$student_id", compact('profileImageUrl', 'student_id'));
+    }
+
+    abort(404);
+});
